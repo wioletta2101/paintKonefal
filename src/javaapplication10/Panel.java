9476bc2fcbb3a23061526d79cc31c46d7c60a1b9
student <student@240-06.INFORMATYKA.local>
2016-12-01 11:18:05 +0100

    linia

--- src/javaapplication10/Panel.java
+++ src/javaapplication10/Panel.java
@@ -3,14 +3,18 @@ package javaapplication10;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-public class Panel extends JPanel implements MouseListener{
+public class Panel extends JPanel implements MouseListener, MouseMotionListener{
 
     private int x=-1;
     private int y=-1;
+    private int z=-1;
+    private int p=-1;
     
  public Panel()
  {
     addMouseListener(this);
+    addMouseMotionListener(this);
+
  }
  public void mouseExited(MouseEvent e)
  {
@@ -22,29 +26,59 @@ public class Panel extends JPanel implements MouseListener{
  }
  public void mousePressed(MouseEvent e)
  {
-//     System.out.println("mousePressed");
+          x=e.getX();
+          y=e.getY();
+          //repaint(); 
+     System.out.println("mousePressed");
+      
  }
  public void mouseReleased(MouseEvent e)
  {
-//     System.out.println("mouseReleased");
+     z=e.getX();
+      p=e.getY();
+      repaint();
+     
+     System.out.println("mouseReleased");
  }
 
  public void mouseClicked(MouseEvent e)
  {
-     x=e.getX();
-     y=e.getY();
-     repaint(); 
+     
+//     x=e.getX();
+//     y=e.getY();
+//     repaint(); 
+
 //    JOptionPane.showMessageDialog(null,
 //   e.getPoint().toString());
  }
  
  public void paint(Graphics g)
  {
-     
-    g.setColor(Color.RED);
-    if(x!=-1 && y!=-1)
-    g.drawOval(x-25, y-25, 50, 50);
+     //rysowanie kolka
+//    g.setColor(Color.RED);
+//    if(x!=-1 && y!=-1)
 //    g.clearRect(0, 0, getSize().width, getSize().height);
-
+//    g.drawOval(x-25, y-25, 50, 50);
+     
+     //rysowanie linii
+     g.setColor(Color.RED);
+     if(x!=-1 && y!=-1)
+     g.drawLine(x, y, z, p);
+     
  }
+ 
+ public void mouseMoved(MouseEvent e)
+ {
+//    x=e.getX();
+//     y=e.getY();
+////     repaint(); 
+     
+ }
+  public void mouseDragged (MouseEvent e)
+ {
+      
+ }
+ 
+ 
+ 
 }
